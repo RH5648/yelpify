b38952ccd1c7b30e48323d59169166490eba8815
Zichen_Liu <61808498+Chi-chan-Lau@users.noreply.github.com>
2020-12-08 21:27:44 -0800

    Add files via upload
    hybrid model with comment

--- yelpify/data_preparation.py
+++ yelpify/data_preparation.py
@@ -1,33 +1,29 @@
 """
 NAME
     data_preparation
-
 DESCRIPTION
-    This module provides access to functions that prepare data and generate features as needed.
-
+    This module provides access to functions that prepare data
+        and generate features as needed.
 FUNCTIONS
     get_input(url)
         Return the dataframe as downloaded from the url.
-
     prepare_data()
         Return the joined and cleaned dataset.
-
     round_of_rating(number)
         Return the number as rounded to the closest half integer
 """
 
 import pandas as pd
 import numpy as np
+from scipy import sparse
+
 
 def get_input(url):
     """ Utility function to get file from url and read into a dataframe.
-
     Args:
         url: the dropbox link from which to download the raw or cleaned data.
-
     Returns:
         The dataframe.
-
     """
     if '.csv' in url:
         return pd.read_csv(url)
@@ -38,47 +34,60 @@ def get_input(url):
     else:
         raise NotImplementedError("File type not supported")
 
+
 def prepare_data(raw=False, round_ratings=False):
     """ Download and read the dataset.
-
     Args:
         raw: whether to download raw data or to download cleaned data.
         round_ratings: whether to perform round of ratings.
-
     Returns:
         the cleaned dataframe.
-
     """
     print('Downloading input data...')
     if raw:
         # read data
-        review = get_input('https://www.dropbox.com/s/mtln9b6udoydn2h/yelp_academic_dataset_review_sample.csv?dl=1')
-        user = get_input('https://www.dropbox.com/s/pngrptljotqm4ds/yelp_academic_dataset_user.json?dl=1')
-        business = get_input('https://www.dropbox.com/s/w0wy854u5swrhmc/yelp_academic_dataset_business.json?dl=1')
+        review = get_input(
+            'https://www.dropbox.com/s/mtln9b6udoydn2h/yelp_academic \
+            _dataset_review_sample.csv?dl=1')
+        user = get_input(
+            'https://www.dropbox.com/s/pngrptljotqm4ds/yelp_academic \
+            _dataset_user.json?dl=1')
+        business = get_input(
+            'https://www.dropbox.com/s/w0wy854u5swrhmc/yelp_academic \
+            _dataset_business.json?dl=1')
 
         # join datasets
-        review_user = pd.merge(review, user, on = "user_id", how = "left", suffixes=("","_user"))
-        review_user_business = pd.merge(review_user, business, on = "business_id", how = "left", suffixes=("","_business"))
-        review_user_business = review_user_business[['user_id', 'business_id', 'stars', 'text',
-                            'name', 'average_stars',
-                            'name_business', 'stars_business', 'categories', 'state', 'city']]
+        review_user = pd.merge(
+            review, user, on="user_id",
+            how="left", suffixes=("", "_user"))
+        review_user_business = pd.merge(
+            review_user, business, on="business_id",
+            how="left", suffixes=("", "_business"))
+        review_user_business = review_user_business[[
+            'user_id', 'business_id',
+            'stars', 'text',
+            'name', 'average_stars',
+            'name_business', 'stars_business',
+            'categories', 'state', 'city']]
     else:
-        review_user_business = get_input('https://www.dropbox.com/s/0c9zandfdsn4ujj/data_clean.parquet?dl=1')
+        review_user_business = get_input(
+            'https://www.dropbox.com/s/0c9zandfdsn4ujj/data_clean.parquet?dl=1'
+            )
     if round_ratings:
         # bucketize numeric features to reduce dimensions
-        review_user_business['average_stars'] = review_user_business['average_stars'].apply(lambda x: round_of_rating(x))
-        review_user_business['stars_business'] = review_user_business['stars_business'].apply(lambda x: round_of_rating(x))
+        review_user_business['average_stars'] = review_user_business[
+            'average_stars'].apply(lambda x: round_of_rating(x))
+        review_user_business['stars_business'] = review_user_business[
+            'stars_business'].apply(lambda x: round_of_rating(x))
     return review_user_business
+
 
 # feature engineering
 def round_of_rating(number):
     """Round a number to the closest half integer.
-
     Args:
         number: input of a number
-
     Returns: the closest half integer to this number.
-
     Example:
     >>> round_of_rating(1.3)
     1.5
@@ -88,6 +97,51 @@ def round_of_rating(number):
     3.0
     >>> round_of_rating(4.1)
     4.0
-
     """
     return round(number * 2) / 2
+
+
+def prepare_data_features(raw=False, round_ratings=False):
+    """ Download, read and modify the dataset.
+    Args:
+        raw: whether to download raw data or to download cleaned data.
+        round_ratings: whether to perform round of ratings.
+    Returns:
+        the modified dataframe.
+    """
+    df = prepare_data(raw=False)
+    df_categories = df['categories'].str.get_dummies(sep=", ")
+    df_categories = df_categories[df_categories.columns[
+        df_categories.sum() > len(df)*0.01]]
+    df = pd.concat([df.drop('categories', 1), df_categories], axis=1)
+    return df
+
+
+def feature_matrix(df, user_id=None, item_id=None):
+    """ Extract users and items features in the dataframe
+    Args:
+        df: The dataframe to be processed.
+        user_id: The id string of a specific user.
+        item_id: The id string of a specific item.
+    Returns:
+        user feature matrix and item features matrix (csr matrix)
+        user_x, item_x are index numbers of the input user id and item id
+    """
+    df1 = df.drop_duplicates(subset=['user_id'], keep='first', inplace=False)
+    user_x = None
+    if user_id is not None:
+        user_x = int(np.argwhere(df1['user_id'].values == user_id))
+    user_features = df1[['average_stars']].values
+    csr_user_features = sparse.csr_matrix(user_features)
+
+    df2 = df.drop_duplicates(
+        subset=['business_id'],
+        keep='first',
+        inplace=False)
+    item_x = None
+    if item_id is not None:
+        item_x = int(np.argwhere(df2['business_id'].values == item_id))
+    item_features = df2.iloc[:, 10:].values
+
+    csr_item_features = sparse.csr_matrix(item_features)
+    return csr_user_features, csr_item_features, user_x, item_x
---
+++ yelpify/recommend_hybrid.py
@@ -0,0 +1,195 @@
+"""
+NAME
+    recommend_hybrid
+DESCRIPTION
+    This module provides access to functions that make recommendations to both
+        new and known item or user.
+FUNCTIONS
+    recommend_hybrid_item(
+        df, model, interactions, item_id, user_dict, item_dict, topn, show)
+        Return the recommended users to an existing item.
+    recommend_hybrid_user(
+        df, model, interactions, user_id, user_dict,
+        item_dict, topn, new_only, threshold, show)
+        Return the recommended items to an existing user.
+    recommend_hybrid_new_item(
+        df, model, interactions, new_item_features,
+        user_dict, item_dict, topn, show)
+        Return the recommended items to a new item.
+    recommend_hybrid_new_user(
+        df, model, interactions, new_user_features,
+        user_dict, item_dict, topn, show)
+        Return the recommended items to a new user.
+"""
+
+import pandas as pd
+import numpy as np
+from yelpify.data_preparation import feature_matrix
+from scipy import sparse
+
+
+# function to make prediction for known items
+def recommend_hybrid_item(
+                         df, model, interactions, item_id,
+                         user_dict, item_dict, topn, show=True):
+    """Funnction to produce a list of top N interested users for a given item. Hybrid version of recommend_known_item
+    Args:
+        df: The orginal data frame
+        model: Trained matrix factorization model
+        interactions: dataset used for training the model
+        item_id: item ID for which we need to generate recommended users
+        user_dict: Dictionary type input containing user_id as key and interaction_index as value
+        item_dict: Dictionary type input containing item_id as key and item_name as value
+        topn: Number of users needed as an output
+        show: whether to show the result of function
+    Returns:
+        user_list: List of recommended users
+    """
+    print('Recommending users for item {}...'.format(item_id))
+    n_users, n_items = interactions.shape
+    user_features, item_features, _, item_x = feature_matrix(
+        df, item_id=item_id)
+    scores = pd.Series(model.predict(
+        item_x, interactions.values[:, item_x], user_features=item_features,
+        item_features=user_features))
+
+    user_list = list(interactions.index[scores.sort_values(
+        ascending=False).head(topn).index])
+    if show is True:
+        print("Recommended Users:")
+        counter = 1
+        for i in user_list:
+            print(str(counter) + '- ' + i)
+            counter += 1
+    return user_list
+
+
+def recommend_hybrid_user(
+                         df, model, interactions, user_id, user_dict,
+                         item_dict, topn, new_only=True, threshold=3,
+                         show=True):
+    """Function to produce user recommendations. Hybrid version of recommend_known_user
+    Args:
+        model: trained matrix factorization model
+        interactions: dataset used for training the model
+        user_id: user ID for which we need to generate recommendation
+        user_dict: Dictionary type input containing user_id as key and interaction_index as value
+        item_dict: Dictionary type input containing item_id as key and item_name as value
+        threshold: value above which the rating is favorable in interaction matrix
+        topn: Number of output recommendation needed
+        new_only: whether to only recommend items that users have not visited
+        show: whether to show the result of function
+    Returns:
+        Prints list of items the given user has already visited
+        Prints list of N recommended items  which user hopefully will be interested in
+    """
+    print('Recommending items for user {}...'.format(user_id))
+    n_users, n_items = interactions.shape
+    user_features, item_features, user_x, _ = feature_matrix(
+        df, user_id=user_id)
+
+    scores = pd.Series(model.predict(
+        user_x, interactions.values[user_x, :], user_features=user_features,
+        item_features=item_features))
+    scores.index = interactions.columns
+    scores = list(pd.Series(scores.sort_values(ascending=False).index))
+    known_items = list(pd.Series(
+        interactions.loc[user_id, :]
+        [interactions.loc[user_id, :] > threshold].index).sort_values(
+            ascending=False))
+    if new_only:
+        scores = [x for x in scores if x not in known_items]
+    item_list = scores[:topn]
+    known_items = list(pd.Series(known_items).apply(lambda x: item_dict[x]))
+    recommended_items = list(pd.Series(item_list).apply(
+        lambda x: item_dict[x]))
+    if show is True:
+        print("Known Likes:")
+        counter = 1
+        for i in known_items:
+            print(str(counter) + '- ' + i)
+            counter += 1
+
+        print("Recommended Items:")
+        counter = 1
+        for i in recommended_items:
+            print(str(counter) + '- ' + i)
+            counter += 1
+    return item_list
+
+
+def recommend_hybrid_new_item(
+                             df, model, interactions, new_item_features,
+                             user_dict, item_dict, topn, show=True):
+    """Funnction to produce a list of top N interested users for a new item.
+    Args:
+        df: The orginal data frame
+        model: Trained matrix factorization model
+        interactions: dataset used for training the model
+        item_id: item ID for which we need to generate recommended users
+        user_dict: Dictionary type input containing user_id as key and interaction_index as value
+        item_dict: Dictionary type input containing item_id as key and item_name as value
+        topn: Number of users needed as an output
+        show: whether to show the result of function
+    Returns:
+        user_list: List of recommended users
+    """
+    print('Recommending users for new items')
+    n_users, n_items = interactions.shape
+    user_features, item_features, _, _ = feature_matrix(df)
+
+    csr_new_item_features = sparse.csr_matrix(new_item_features)
+    scores = pd.Series(model.predict(
+        0, np.zeros(n_users), user_features=csr_new_item_features,
+        item_features=user_features))
+
+    user_list = list(interactions.index[
+        scores.sort_values(ascending=False).head(topn).index])
+    if show is True:
+        print("Recommended Users:")
+        counter = 1
+        for i in user_list:
+            print(str(counter) + '- ' + i)
+            counter += 1
+    return user_list
+
+
+def recommend_hybrid_new_user(
+                             df, model, interactions, new_user_features,
+                             user_dict, item_dict, topn, new_only=False,
+                             threshold=3, show=True):
+    """Function to produce user recommendations.
+    Args:
+        model: trained matrix factorization model
+        interactions: dataset used for training the model
+        user_id: user ID for which we need to generate recommendation
+        user_dict: Dictionary type input containing user_id as key and interaction_index as value
+        item_dict: Dictionary type input containing item_id as key and item_name as value
+        threshold: value above which the rating is favorable in interaction matrix
+        topn: Number of output recommendation needed
+        new_only: whether to only recommend items that users have not visited
+        show: whether to show the result of function
+    Returns:
+        Prints list of N recommended items  which user hopefully will be interested in
+    """
+    print('Recommending items for new users')
+    n_users, n_items = interactions.shape
+    csr_new_user_features = sparse.csr_matrix(new_user_features)
+
+    user_features, item_features, _, _ = feature_matrix(df)
+    scores = pd.Series(model.predict(
+        0, np.zeros(n_items), user_features=csr_new_user_features,
+        item_features=item_features))
+    scores.index = interactions.columns
+    scores = list(pd.Series(scores.sort_values(ascending=False).index))
+
+    item_list = scores[:topn]
+    recommended_items = list(pd.Series(item_list).apply(
+        lambda x: item_dict[x]))
+    if show is True:
+        print("Recommended Items:")
+        counter = 1
+        for i in recommended_items:
+            print(str(counter) + '- ' + i)
+            counter += 1
+    return item_list
