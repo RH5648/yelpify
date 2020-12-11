613e9a33c80df5fbb0f7e765c355e184fa26e752
RH5648 <61851052+RH5648@users.noreply.github.com>
2020-12-11 08:17:35 -0800

    test hybrid

--- yelpify/data_preparation.py
+++ yelpify/data_preparation.py
@@ -118,6 +118,7 @@ def prepare_data_features(raw=False, round_ratings=False):
         the modified dataframe.
     """
     df = prepare_data(raw=False)
+    print("prepare features")
     df_categories = df['categories'].str.get_dummies(sep=", ")
     df_categories = df_categories[df_categories.columns[
         df_categories.sum() > len(df)*0.03]]
@@ -135,6 +136,7 @@ def feature_matrix(df, user_id=None, item_id=None):
         user feature matrix and item features matrix (csr matrix)
         user_x, item_x are index numbers of the input user id and item id
     """
+    print("get feature matrix")
     df1 = df.drop_duplicates(subset=['user_id'], keep='first', inplace=False)
     user_x = None
     if user_id is not None:
