fe2b2d9c2caee4d271d4527e674273fd1c3b7f4f
RH5648 <61851052+RH5648@users.noreply.github.com>
2020-12-12 00:32:07 -0800

    Update test_recommend_hybrid.py

--- codebase/tests/test_recommend_hybrid.py
+++ codebase/tests/test_recommend_hybrid.py
@@ -1,11 +1,10 @@
 import os
 import unittest
-
-
+import numpy as np
 import codebase
 from yelpify.model_hybrid import train_model
-from yelpify.recommend_hybrid import recommend_hybrid_item,\
-    recommend_hybrid_user
+from yelpify.recommend_hybrid import recommend_hybrid_item, \
+    recommend_hybrid_new_item
 from yelpify.data_preparation import prepare_data_features
 
 USER_ID = "avXKk5RYsDWeRgkHv1wfGQ"
@@ -13,29 +12,20 @@ ITEM_ID = "VMPSdoBgJuyS9t_x_caTig"
 data_path = os.path.join(codebase.__path__[0], 'data')
 df = prepare_data_features(raw=False)
 
-model_full, df_interactions, user_dict, item_dict = train_model(
-    df=df,
-    user_id_col='user_id',
-    item_id_col='business_id',
-    item_name_col='name_business',
-    evaluate=True)
+model_full, df_interactions, user_dict, item_dict, \
+    user_feature_map, business_feature_map = train_model(
+        df=df,
+        user_id_col='user_id',
+        item_id_col='business_id',
+        item_name_col='name_business',
+        evaluate=True)
 
 
 class TestModel(unittest.TestCase):
 
     def test_hybrid_item(self):
-        rec_list_user = recommend_hybrid_user(
-            model=model_full,
-            interactions=df_interactions,
-            user_id=USER_ID,
-            user_dict=user_dict,
-            item_dict=item_dict,
-            topn=10,
-            show=True)
-        self.assertEqual(len(rec_list_user), 10)
-
-    def test_evaluate_model(self):
         rec_list_item = recommend_hybrid_item(
+            df=df,
             model=model_full,
             interactions=df_interactions,
             item_id=ITEM_ID,
@@ -44,6 +34,18 @@ class TestModel(unittest.TestCase):
             topn=10,
             show=True)
         self.assertEqual(len(rec_list_item), 10)
+
+    def test_hybrid_new_item(self):
+        rec_list_new_item = recommend_hybrid_new_item(
+            df=df,
+            model=model_full,
+            interactions=df_interactions,
+            new_item_features=np.random.binomial(1, 0.05, size=(1, 89)),
+            user_dict=user_dict,
+            item_dict=item_dict,
+            topn=10,
+            show=True)
+        self.assertEqual(len(rec_list_new_item), 10)
 
 
 if __name__ == "__main__":
